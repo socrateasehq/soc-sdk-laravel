a57d46959e5e489deecc48703a3192440878ef11
Rishabh Jain <rishabh@socratease.co>
2023-06-28 13:41:34 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,31 @@
+==============================================
+Socratease SDK in a Laravel Project
+==============================================
+
+A Laravel project which shows the working of the Socratease SDK.
+
+# Installation
+
+Install Laravel 4.5 or higher
+
+Steps to install Laravel
+
+::
+
+    Visit this url https://kinsta.com/knowledgebase/install-laravel/
+
+# Run application
+
+::
+
+    $ php artisan serve
+
+Then, if you go to the URL http://127.0.0.1:8000/ in your web browser, and you see
+Your Home Page on the screen, you know everything is working as expected.
+
+Once the project is up and running, you can go to the URL http://127.0.0.1:8000/assessments/cms/home to see how the
+Socratease SDK works. You can change the parameters in the .env file and see how they affect the SDK.
+
+This video\_ shows you how to install and run the project.
+
+.. \_video: https://www.loom.com/share/0af61f8690e34f5ebeae2eff7c4e9a17
---
+++ routes/web.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+ */
+
+Route::get('/assessments/{any}', function ($any) {
+ $payload_array = [
+  'username'     => 'new_user@gmail.com',
+  'user_details' => [
+   'first_name' => 'First',
+   'last_name'  => 'Last',
+   'email'      => 'new_user@gmail.com',
+  ],
+  'route_prefix' => "/assessments",
+ ];
+
+ $payload_string = json_encode($payload_array, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+
+ $hmac_payload = hash_hmac(
+  'sha256',
+  $payload_string,
+  env('SOC_CLIENT_SECRET'),
+  true
+ );
+
+ $hmac_payload_base_encoded = base64_encode($hmac_payload);
+
+ $soc_allowed_content_screens = explode(",", env('SOC_ALLOWED_CONTENT_SCREENS'));
+
+ return view('socratease-entry', [
+  'payload_string'              => $payload_string,
+  'hmac_payload'                => $hmac_payload_base_encoded,
+  'soc_allowed_content_screens' => $soc_allowed_content_screens,
+  'soc_version'                 => env('SOC_VERSION'),
+  'soc_client_id'               => env('SOC_CLIENT_ID'),
+  'soc_route_prefix'            => env('SOC_ROUTE_PREFIX'),
+  'brand_name'                  => env('BRAND_NAME'),
+  'soc_api_host'                => env('SOC_API_HOST'),
+ ]);
+
+})->where('any', '.*');
+
+Route::get('/', function () {
+ return view('welcome');
+});
